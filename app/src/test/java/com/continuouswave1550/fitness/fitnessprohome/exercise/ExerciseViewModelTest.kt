5266fad89b4e872170023d8b3688106e847d3a45
James Bray <jrbrayjr2006@gmail.com>
2020-10-18 20:57:02 -0500

    Add styled icon and first unit test stub

---
+++ app/src/test/java/com/continuouswave1550/fitness/fitnessprohome/exercise/ExerciseViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.continuouswave1550.fitness.fitnessprohome.exercise
+
+import org.junit.jupiter.api.BeforeEach
+import javax.inject.Inject
+
+class ExerciseViewModelTest {
+    
+    var viewModel: ExerciseViewModel = ExerciseViewModel()
+
+    @BeforeEach
+    fun setUp() {}
+}
